ea85d738da4d043bff2c35509ea560326df9004a
mateustech <mateustech686@gmail.com>
2021-12-18 13:43:42 -0300

    fix(responsive): adjusts breackpoints

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
       <SeoHead />
       <div className="w-full h-screen bg-primary flex items-center justify-center">
         <div className="absolute w-screen h-20 top-0 pl-12 lg:pl-20 py-4">
-          <div className="h-[10rem] w-[8rem] lg:h-[5rem] lg:w-[10rem] relative ">
+          <div className="h-[10rem] w-[8rem] lg:h-[3rem] lg:w-[8rem] relative ">
             <Image
               layout="fill"
               objectFit="contain"
@@ -19,11 +19,8 @@ const Home: NextPage = () => {
             />
           </div>
         </div>
-        <Tilt
-          options={{ max: 10 }}
-          className="flex items-center justify-center lg:space-x-16 glass p-8 rounded-lg relative"
-        >
-          <h1 className="text-2xl lg:text-6xl font-bold text-white text-right">
+        <div className="flex items-center justify-center lg:space-x-16 glass p-8 rounded-lg relative">
+          <h1 className="text-2xl lg:text-4xl font-bold text-white text-right">
             Solução
             <br />
             Inteligente
@@ -32,18 +29,21 @@ const Home: NextPage = () => {
             <br />
             De Equipes.
           </h1>
-          <div className="h-[10rem] w-[8rem] lg:h-[30rem] lg:w-[30rem] relative ">
+          <Tilt
+            options={{ max: 10 }}
+            className="h-[10rem] w-[8rem] lg:h-[24rem] lg:w-[24rem] relative "
+          >
             <Image
               layout="fill"
               objectFit="contain"
               src="/screen.png"
               alt="Simbolo planus"
             />
-          </div>
-        </Tilt>
+          </Tilt>
+        </div>
         <div
           className="w-full bottom-10 text-2xl animate-bounce absolute flex items-center justify-center text-white"
-          style={{ height: "70px" }}
+          style={{ height: "10px" }}
         >
           <BsFillMouse2Fill />
         </div>
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
               alt="Simbolo planus"
             />
           </div>
-          <h1 className="text-2xl lg:text-6xl font-bold text-primary text-right">
+          <h1 className="text-2xl lg:text-4xl font-bold text-primary text-right">
             Página Em Construção...
           </h1>
         </div>
